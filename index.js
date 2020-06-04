ef6a266e816d59b936bb9a1881206e66e6634df2
Waverley Leung <waverley.nyc@gmail.com>
2020-06-04 22:20:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 function destructivelyAppendKitten(name) {
   return kittens.push("Ralph")
 }
+
+function destructivelyPrependKitten(name) {
+  return kittens.unshift("Bob")
+}
