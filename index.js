b8a9a847254faf28deebdcb4648b5d973cacb1a5
Waverley Leung <waverley.nyc@gmail.com>
2020-06-04 22:41:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,29 +1,33 @@
 var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
-
+//adds to end of og
 function destructivelyAppendKitten(name) {
   return kittens.push("Ralph")
 }
-
+//adds to beginning of og
 function destructivelyPrependKitten(name) {
   return kittens.unshift("Bob")
 }
-
+//remove last from og
 function destructivelyRemoveLastKitten() {
   kittens.pop()
   return kittens
 }
-
+//remove first from og
 function destructivelyRemoveFirstKitten() {
   kittens.shift()
   return kittens
 }
-
+//add to end & new array
 function appendKitten(name) {
   return kittens.concat(name)
 }
-
+//add to beginning & new array
 function prependKitten(name) {
   return [name, ...kittens]
+}
+//removes the last kitten in og array, returns a new array & leaves og alone
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1)
 }
